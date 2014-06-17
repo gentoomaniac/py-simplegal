64301de3372c346a50c16903c36188f6076343a6
Marco Siebecke <gentoomaniac@gmail.com>
2014-06-17 17:19:13 +0200

    removed redundancies in template and controller by including

--- py-simplegal/pysimplegal/controllers/view.py
+++ py-simplegal/pysimplegal/controllers/view.py
@@ -14,6 +14,19 @@ log = logging.getLogger(__name__)
 
 class ViewController(BaseController):
 
+    def _set_global_template_vars(self, path):
+        c.site_name = config['app_conf']['site_name']
+        c.site_template = config['app_conf']['site_template']
+        c.photo_store = config['app_conf']['photo_store']
+        c.folder_preview = config['app_conf']['folder_preview']
+        c.path = path
+        if path:
+            c.path += '/'
+        c.paths = h.path_to_array(path)
+        c.thumb_height = config['app_conf']['thumb_height']
+        c.thumb_width = config['app_conf']['thumb_width']
+
+
     def viewfolder(self, path=''):
         if path:
             abs_path = "%s/%s" % (config['app_conf']['photo_store'], path)
@@ -25,21 +38,9 @@ class ViewController(BaseController):
         content = dirs + files
 
         # add template vars
-        c.site_name = config['app_conf']['site_name']
-        c.site_template = config['app_conf']['site_template']
-        c.photo_store = config['app_conf']['photo_store']
-        c.folder_preview = config['app_conf']['folder_preview']
-        c.current_path = path
-        c.paths = h.path_to_array(path)
+        self._set_global_template_vars(path=path)
         c.abspath = abs_path
-        c.path = path
-        if path:
-            c.path += '/'
-
         c.content = content
-
-        c.thumb_height = config['app_conf']['thumb_height']
-        c.thumb_width = config['app_conf']['thumb_width']
 
         return render("%s/viewfolder.html" % config['app_conf']['site_template'])
 
@@ -51,15 +52,10 @@ class ViewController(BaseController):
         else:
             abs_path = config['app_conf']['photo_store']
 
-        c.site_name = config['app_conf']['site_name']
-        c.site_template = config['app_conf']['site_template']
+
+        # add template vars
+        self._set_global_template_vars(path=path)
         c.abspath = abs_path
-        c.path = path
-        c.paths = h.path_to_array(path)
         c.mime = h.get_file_type(abs_path)['mime']
-
-
-        if path:
-            c.path += '/'
 
         return render("%s/viewvideo.html" % config['app_conf']['site_template'])
